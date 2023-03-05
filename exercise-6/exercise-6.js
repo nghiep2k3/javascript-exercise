41b5c8852ae75d8a92a12456c02dc387f9b79d25
Nguyễn Nghiệp <nghiep2k3>
2023-03-06 00:40:39 +0700

    ex

--- exercise-6/exercise-6.js
+++ exercise-6/exercise-6.js
@@ -1,6 +1,16 @@
 function convertNumber(number) {
   // Coding here
+  // Math.truc trả về số nguyên loại bỏ thập phân
+  var x = [];
+  while (number > 0) {
+    x.push(number  % 2);  
+    number = Math.trunc((number / 2));
+  }
+  return x.reverse().join("");
 }
+
+console.log(convertNumber(47));
+
 
 
 
